4348ffc9a4732399930f0aa41960116d537b5569
Idioticalduck <rafarukmanto@gmail.com>
2026-04-12 04:40:33 -0400

    moved over nick's code(they way he commited into main i just copy pasted the folder really) into the building branch, updated some nav2 params and added more functionality to camera-to-points

---
+++ src/my_robot_cam2points/my_robot_cam2points/depend/cam_dependencies.py
@@ -0,0 +1,198 @@
+import numpy as np 
+import cv2
+# import pandas as pd
+from sensor_msgs.msg import PointCloud2, PointField
+import struct
+
+
+#sample dict format
+details = {
+    # details about the camera
+    "camera_focal": 2.8,
+    "sensor_height": 3.6,
+    "sensor_width": 4.8,
+    "image_w": 1920,
+    "image_h": 1080,
+
+    # GPS & camera positions (meters)
+    "gps_location": np.array([[0], [0], [0]]),
+    "camera_location": np.array([[1], [0.5], [0]]),
+
+    # camera distortion constants
+    "k1": 0,
+    "k2": 0,
+    "k3": 0,
+    "p1": 0,
+    "p2": 0,
+}
+
+
+
+def create_matrixes(details,vehicle_tilt,vehicle_slant,vehicle_heading,relativeto='base'):
+    #im assuming that the inputs will be in degrees, if the input is in radianss, this segment will be removed
+    # vehicle_heading = np.radians(vehicle_heading)
+    # vehicle_slant   = np.radians(vehicle_slant)
+    # vehicle_tilt    = np.radians(vehicle_tilt)
+    camera_rotation_on_mount  = np.radians(details.camera_rotation_on_mount)
+    camera_vertical_rotation = np.radians(details.camera_vertical_rotation)
+    camera_tilt               = np.radians(details.camera_tilt)
+
+    # Combined angles
+    y_heading = camera_rotation_on_mount #+ vehicle_heading
+    vertical  =  camera_vertical_rotation #vehicle_slant +
+    tilt      =  camera_tilt#vehicle_tilt +
+    #rotation matrix to be able to project based on rotation of the camera 
+    #used because in practice the vehicle wilal use GPS
+    
+    #Yaw
+    R   = np.array([
+        [-np.sin(y_heading), np.cos(y_heading), 0],
+        [0, 0, 1],
+        [np.cos(y_heading), np.sin(y_heading), 0]
+    ])
+
+
+    #Pitch
+    R_z = np.array([
+        [np.cos(vertical), -np.sin(vertical), 0],
+        [np.sin(vertical),  np.cos(vertical), 0],
+        [0, 0, 1]
+    ])
+
+    #Roll
+    R_x = np.array([
+        [1, 0, 0],
+        [0, np.cos(tilt), -np.sin(tilt)],
+        [0, np.sin(tilt),  np.cos(tilt)]
+    ])
+
+    # Offset matrix
+    camera_offset= details.camera_location- details.gps_location if relativeto=='gps' else [[0],[0],[0 ]]
+    camera_offset_matrix =  _camera_offset_matrix(vehicle_slant, vehicle_heading, vehicle_tilt)
+    camera_offset_matrix = camera_offset #camera_offset_matrix @ 
+    R_inv, R_z_inv, R_x_inv = np.linalg.inv(R), np.linalg.inv(R_z), np.linalg.inv(R_x)
+    return R, R_inv, R_x_inv, R_z_inv, camera_offset_matrix
+
+    
+
+def _camera_offset_matrix(vehicle_slant, vehicle_heading, vehicle_tilt):
+    # Returns 3x3 GPS offset matrix
+    return np.array([
+        [
+            np.cos(vehicle_slant)*np.cos(vehicle_heading),
+            np.cos(vehicle_slant)*np.sin(vehicle_heading)*np.sin(vehicle_tilt) - np.sin(vehicle_slant)*np.cos(vehicle_tilt),
+            np.cos(vehicle_slant)*np.sin(vehicle_heading)*np.cos(vehicle_tilt) + np.sin(vehicle_slant)*np.sin(vehicle_tilt)
+        ],
+        [
+            np.sin(vehicle_slant)*np.cos(vehicle_heading),
+            np.sin(vehicle_slant)*np.sin(vehicle_heading)*np.sin(vehicle_tilt) + np.cos(vehicle_slant)*np.cos(vehicle_tilt),
+            np.sin(vehicle_slant)*np.sin(vehicle_heading)*np.cos(vehicle_tilt) - np.cos(vehicle_slant)*np.sin(vehicle_tilt)
+        ],
+        [
+            -np.sin(vehicle_heading),
+            np.cos(vehicle_heading)*np.sin(vehicle_tilt),
+            np.cos(vehicle_heading)*np.cos(vehicle_tilt)
+        ]
+    ])
+
+
+# def cam_to_gps(details,u,v,D,vehicle_heading=0,vehicle_slant=0,vehicle_tilt=0,camera_rotation_on_mount=0 ,camera_vertical_rotation=0,camera_tilt=0):
+
+#     N,E,D,=cam_to_coords(details,u,v,D,vehicle_heading,vehicle_slant,vehicle_tilt,camera_rotation_on_mount,camera_vertical_rotation,camera_tilt,'gps')
+    
+#     #GPS conversion
+    
+#     # convert ENU -> geodetic
+#     lat, lon, alt = pm.enu2geodetic(N,E,D, lat0, lon0, alt0)
+#     print(lat0,lon0,alt0)
+#     print(lat, lon, alt)
+#     return (lat,lon,alt)
+def cam_to_coords(details,u,v,D,vehicle_heading=0,vehicle_slant=0,vehicle_tilt=0,camera_rotation_on_mount=0,camera_vertical_rotation=0,camera_tilt=0,relativeto='base'):
+    x_prime,y_prime=camera_to_normalized(u,v,details)
+    R,R_inv,R_x_inv,R_z_inv,camera_offsetmatrix=create_matrixes(details,vehicle_heading,vehicle_slant,vehicle_tilt,camera_rotation_on_mount,camera_vertical_rotation,camera_tilt,relativeto)
+    NE=convert_to_North_East_Down(x_prime,y_prime,D,R,R_inv,R_x_inv,R_z_inv)
+   
+    N=NE[0]-camera_offsetmatrix[2]
+    E=NE[1]+camera_offsetmatrix[0]
+
+    D-=camera_offsetmatrix[1]
+  
+    return(N,E,D)
+    
+#convert back to normalized 
+def camera_to_normalized(u,v,details ):
+    x_doubleprime=(u-details.cx)/details.fx
+    y_doubleprime=(v-details.cy)/details.fy
+
+#undo distortion(if the distortion is removed by the subsystems group already however, this part can be removed)
+    distortion = np.array([details.k1, details.k2, details.p1, details.p2, details.k3])
+    K=np.array([
+        [details.fx,0,details.cx],
+        [0,details.fy,details.cy],
+        [0,0,1]
+    ])
+
+    points = np.stack([x_doubleprime, y_doubleprime], axis=1).astype(np.float32).reshape(-1, 1, 2)                      # shape (N, 1, 2)
+
+    undistorted=cv2.undistortPoints(points,K,distortion,P=K)
+    
+
+    #undistorted normalized coordinates
+    x_prime = undistorted[:, 0, 0]   
+    y_prime = undistorted[:, 0, 1]
+    return x_prime,y_prime
+
+
+
+def convert_to_North_East_Down(x_prime,y_prime,D,R,R_inv,R_x_inv,R_z_inv):
+    zeros=np.zeros(len(x_prime))
+    #Yc is made under the assumption as of now that D is constant, D will later be changed when accessing lidar camera
+    Yc=(R @ np.array([zeros,zeros,D]))[1]
+
+    #Zc is found intuitively since y'=Yc/Zc so can get y'/Yc= 1/Zc so Zc= Yc/y'
+    Zc=(Yc/y_prime)
+    Xc=(Zc*x_prime)#inverse of x'=Xc/Zc
+
+
+    #R is inversed here to be able to do the opposite of distance to camera
+  
+
+
+    # empty zeros is used instead of Yc as down is already known so no need to recompute it 
+    c=np.array([
+        Xc,
+        zeros,
+        Zc
+    ])
+    c=R_x_inv @ c 
+    c=R_z_inv @ c
+
+     #getting make the North East Down Matrix 
+    c=R_inv @ c
+    return c
+
+
+# u=np.array([455.798365672726,480,460])
+# v= np.array([335.93520795940526,220,340])
+# D=np.array([0.958309-0.092417]*len(u))
+
+# print("CAM TO COORDS")
+# cam_to_coords(u,v,D,0,0,0)
+
+
+def make_cloud(node,points):
+    cloud = PointCloud2()
+    cloud.header.frame_id='base_link'
+    cloud.header.stamp = node.get_clock().now().to_msg()
+    cloud.height = 1
+    cloud.width = len(points)
+    cloud.fields = [
+        PointField(name='x', offset=0,  datatype=PointField.FLOAT32, count=1),
+        PointField(name='y', offset=4,  datatype=PointField.FLOAT32, count=1),
+        PointField(name='z', offset=8,  datatype=PointField.FLOAT32, count=1),
+    ]
+    cloud.is_bigendian = False
+    cloud.point_step = 12
+    cloud.row_step = 12 * len(points)
+    cloud.data = b''.join(struct.pack('fff', x, y, z) for x, y, z in points)
+    return cloud
---
+++ src/my_robot_cam2points/my_robot_cam2points/nodes/cam_coordsNode.py
@@ -0,0 +1,159 @@
+import rclpy
+import numpy as np
+import  depend.cam_dependencies as cam_dependencies
+from rclpy.node import Node
+import numpy as np
+from sensor_msgs.msg import NavSatFix,PointCloud2,PointField
+from std_msgs.msg import Float32
+from vision_msgs.msg import Detection2D
+import struct
+#from yolop_lane_ros2.msg import LaneData
+
+#These are the functions that will be used by the node to make Camera to Coords.
+#The node has not been created to allow me to accomodate to subscribing to the nodes to get depth and lane line data
+
+
+
+
+#assumes left would be negative and is implemented because of the camera being put on an adjustable mount. makes it able to 
+#make adjustments based on camera rotation. 
+
+class cam_coordsNode(Node):
+    def __init__(self):
+        super().__init__('camtocoords')
+
+        self.lanecoordspublisher_L = self.create_publisher(
+            PointCloud2,
+            '/relative_lanecoords_L',
+            10
+        )
+        self.lanecoordspublisher_L = self.create_publisher(
+            PointCloud2,
+            '/relative_lanecoords_L',
+            10
+        )
+        
+        self.pothole = self.create_publisher(
+             PointCloud2,
+             'waypoint',
+             10
+         )
+        self.gpscubscription= self.create_subscription(
+            NavSatFix,
+            '/gps/fix',
+            self.gpscallback,
+            10
+        )
+        # self.lanedetectionsubscription = self.create_subscription(
+        #     LaneData,          # message type we're receiving
+        #     'overlay_data',          # no topic name because no working node yet
+        #     self.camLane_callback,  # callback when message arrives
+        #     10                  # queue size / history depth
+        # )
+        
+
+        self.cam2dsubscription=self.create_subscription(
+            Detection2D(),
+            "/camera/detections2d",
+            self.objcallback
+        )
+   
+        self.orientation=np.array[0,0,0]
+        
+        self.camDetails={
+    # details about the camera
+    "camera_focal": 2.8,
+    "sensor_height": 3.6,
+    "sensor_width": 4.8,
+    "image_w": 1920,
+    "image_h": 1080,
+
+    # GPS & camera positions (meters)
+    "gps_location": np.array([[0], [0], [0]]),
+    "camera_location": np.array([[1], [0.5], [0]]),
+
+    # camera distortion constants
+    "k1": 0,
+    "k2": 0,
+    "k3": 0,
+    "p1": 0,
+    "p2": 0,
+    "camera_rotation_on_mount":0 ,
+    "camera_verticarotation":0,
+    "camera_tilt":0
+    }
+      
+
+
+    def objcallback(self,msg):
+        potcoords=[[],[]]
+        potholes=list(filter(lambda obj: obj.class_id=="pothole", msg.detections))
+        pothole_radius=0.6 #meters
+        if(len(potholes)>0):
+            for pothole in potholes:
+                u = pothole.bbox.center.position.x
+                v = pothole.bbox.center.position.y
+                
+                N, E, _ = cam_dependencies.cam_to_coords(self.camDetails, u, v,self.orientation[0],self.orientation[1],self.orientation[2])
+                
+                angles = np.linspace(0, 2*np.pi, 8)
+                for angle in angles:
+                    x = N + pothole_radius * np.cos(angle)
+                    y = E + pothole_radius * np.sin(angle)
+                    potcoords.append([x, y, 0.0])
+
+        cloud = cam_dependencies.make_cloud('potholecoords',potcoords)
+        self.pothole_pub.publish(cloud)
+        """
+       Im just assuming its in a in32 array sorted like this
+       [[x1],[y1],[x2],[y2]]
+       """
+
+
+
+    def Imu_callback(self,msg):
+        self.orientation=[msg.x,msg.y.msg.z]      
+    def camLane_callback(self,msg):
+        L=([[],[],[]])
+        R=([[],[],[]])
+        N,E,_=cam_dependencies.cam_to_coords(self.camDetails,msg.left_lane_x,msg.left_lane_y,self.orientation[0],self.orientation[1],self.orientation[2])
+        N2,E2,_=cam_dependencies.cam_to_coords(self.camDetails,msg.right_lane_x,msg.right_lane_y,self.orientation[0],self.orientation[1],self.orientation[2])
+        
+
+        for x in range (len(N)):
+            L[0].append(N[x])
+            L[1].append(-E[x])
+            L[2].append(0)
+        for x in range (len(N2)):
+            R[0].append(N2[x])
+            R[1].append(-E2[x])
+            R[2].append(0)
+        L= zip(L[0], L[1], L[2])
+        R= zip(R[0], R[1], R[2]) 
+        L_cloud=cam_dependencies.make_cloud(L)
+        R_cloud=cam_dependencies.make_cloud(R)
+        self.lanecoordspublisher_L.publish(self,L_cloud)
+        self.lanecoordspublisher_R.publish(self,R_cloud)
+
+
+# Entry point of the program
+def main():
+    # Initialize the ROS 2 system
+    rclpy.init()
+
+    # Create an instance of our node
+    node = cam_coordsNode()
+
+    # Keep the node alive and processing callbacks
+    rclpy.spin(node)
+
+    # Clean up once the node stops
+    node.destroy_node()
+
+    # Shut down ROS 2
+    rclpy.shutdown()
+
+
+# Only run main() if this file is executed directly
+if __name__ == '__main__':
+    main()
