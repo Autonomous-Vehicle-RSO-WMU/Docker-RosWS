9c3397ec9031c256c8c5db7491b52698aa3fb7f1
Idioticalduck <152240767+Idioticalduck@users.noreply.github.com>
2026-05-25 13:31:08 -0400

    Enhance cam_coordsNode with center lane publisher + small tweaks
    Added a new publisher for center lane coordinates and updated pothole and lane detection handling.

--- src/my_robot_cam2points/my_robot_cam2points/nodes/cam_coordsNode.py
+++ src/my_robot_cam2points/my_robot_cam2points/nodes/cam_coordsNode.py
@@ -37,16 +37,21 @@ class cam_coordsNode(Node):
             '/relative_lanecoords_R',
             qos_profile
         )
+        self.lanecoordspublisher_M = self.create_publisher(
+            PointCloud2,
+            '/relative_lanecoords_Center',
+            qos_profile
+        )
         
         self.pothole = self.create_publisher(
              PointCloud2,
-             'waypoint',
+             '/potholes',
              qos_profile
          )
 
         self.lanedetectionsubscription = self.create_subscription(
             LaneData,          # message type we're receiving
-            'overlay_data',          # no topic name because no working node yet
+            'overlay_data',         
             self.camLane_callback,  # callback when message arrives
             qos_profile
         )
@@ -68,10 +73,10 @@ class cam_coordsNode(Node):
     "image_h": 1080,
 
     # GPS & camera positions (meters)
-    "camera_location": np.array([[0.25], [0], [1.01]]),
+    "camera_location": np.array([[0.381], [0], [1.01]]),
 
     "camera_rotation_on_mount":0 ,
-    "camera_verticalrotation":35,
+    "camera_verticalrotation":0.6283,
 
     }
       
@@ -107,23 +112,31 @@ class cam_coordsNode(Node):
     def camLane_callback(self,msg):
         L=([[],[],[]])
         R=([[],[],[]])
+        MID=([[],[],[]])
         N,E,_=cam_dependencies.cam_to_coords(self.camDetails,np.array(msg.left_lane_x),np.array(msg.left_lane_y))
         N2,E2,_=cam_dependencies.cam_to_coords(self.camDetails,np.array(msg.right_lane_x),np.array(msg.right_lane_y)  )
-
+        N3,E3,_=cam_dependencies.cam_to_coords(self.camDetails,np.array(msg.center_x),np.array(msg.center_y)  )
         for x in range (len(N)):
-            L[0].append(N[x])
+            L[0].append(N[x]+self.camDetails.cameralocation[0][0])
             L[1].append(-E[x])
             L[2].append(0)
         for x in range (len(N2)):
-            R[0].append(N2[x])
+            R[0].append(N2[x]+self.camDetails.cameralocation[0][0])
             R[1].append(-E2[x])
             R[2].append(0)
+        for x in range (len(N3)):
+            MID[0].append(N3[x]+self.camDetails.cameralocation[0][0])
+            MID[1].append(-E3[x])
+            MID[2].append(0)
         L= zip(L[0], L[1], L[2])
         R= zip(R[0], R[1], R[2]) 
+        MID= zip(MID[0], MID[1], MID[2]) 
         L_cloud=cam_dependencies.make_cloud(self,L)
         R_cloud=cam_dependencies.make_cloud(self,R)
+        M_cloud=cam_dependencies.make_cloud(self,MID)
         self.lanecoordspublisher_L.publish(L_cloud)
         self.lanecoordspublisher_R.publish(R_cloud)
+        self.lanecoordspublisher_M.publish(M_cloud)
 
 
 # Entry point of the program
